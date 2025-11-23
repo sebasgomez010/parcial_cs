db08e31c9567fe42830499c41bb13a077c0f7058
Sebastian Gomez <144959758+sebasgomez010@users.noreply.github.com>
2025-11-23 15:19:28 -0500

    Refactor LastOrders to use a private immutable list

--- Parcial/Parcial/src/Domain/Services/OrderService.cs
+++ Parcial/Parcial/src/Domain/Services/OrderService.cs
@@ -7,13 +7,25 @@ using Domain.Entities;
 
 public static class OrderService
 {
-    public static List<Order> LastOrders = new List<Order>();
+    // Campo privado inmutable
+    private static readonly List<Order> _lastOrders = new();
+
+    // Propiedad pública solo lectura 
+    public static IReadOnlyList<Order> LastOrders => _lastOrders;
 
     public static Order CreateTerribleOrder(string customer, string product, int qty, decimal price)
     {
-        var o = new Order { Id = new Random().Next(1, 9999999), CustomerName = customer, ProductName = product, Quantity = qty, UnitPrice = price };
-        LastOrders.Add(o);
-        Infrastructure.Logging.Logger.Log("Created order " + o.Id + " for " + customer);
+        var o = new Order
+        {
+            Id = new Random().Next(1, 999999),
+            CustomerName = customer,
+            ProductName = product,
+            Quantity = qty,
+            UnitPrice = price
+        };
+
+        _lastOrders.Add(o);
+        Infrastructure.Logging.Logger.Log($"Created order {o.Id} for {customer}");
         return o;
     }
 }
