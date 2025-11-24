15961e31a74621cc97a515e2f662229926f46876
Sebastian Gomez <144959758+sebasgomez010@users.noreply.github.com>
2025-11-24 17:59:24 -0500

    Comment out unused OrdersController class
    Comment out the OrdersController class to clarify structure.

--- Parcial/Parcial/src/WebApi/Controllers/OrdersController.cs
+++ Parcial/Parcial/src/WebApi/Controllers/OrdersController.cs
@@ -1,10 +1,10 @@
-using System;
+// using System;
 
-// BAD: Mixing minimal APIs with Controllers folder just to confuse structure
-namespace WebApi.Controllers
-{
-    // public static class OrdersController /* No ControllerBase, no attributes: unused on purpose */ 
-    {
-        public static string DoNothing() => "This controller does nothing. Endpoints are in Program.cs";
-    }
-}
+// // BAD: Mixing minimal APIs with Controllers folder just to confuse structure
+// namespace WebApi.Controllers
+// {
+//     public static class OrdersController /* No ControllerBase, no attributes: unused on purpose */ 
+//     {
+//         // public static string DoNothing() => "This controller does nothing. Endpoints are in Program.cs";
+//     }
+// }
